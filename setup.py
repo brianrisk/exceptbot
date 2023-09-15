afc554d99aeb21ac5870650c6fcba80af7e8bd4d
brianrisk <brian@geneffects.com>
2023-09-15 15:37:01 -0400

    updating version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'LICENSE.md'), encoding='utf-8') as f:
 
 setup(
     name='exceptbot',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
