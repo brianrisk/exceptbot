4fa9d8a9a8b8c93fd64f4b59e154ec880865054e
brianrisk <brian@geneffects.com>
2023-12-02 13:12:00 -0500

    adding openai to setup requirements

--- setup.py
+++ setup.py
@@ -12,12 +12,13 @@ with open(path.join(this_directory, 'LICENSE.md'), encoding='utf-8') as f:
 
 setup(
     name='exceptbot',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'django>=3.0',
         'python-decouple>=3.4',
+        'openai==1.3.7'
     ],
     url='https://exceptbot.com',
     author='Brian Risk',
